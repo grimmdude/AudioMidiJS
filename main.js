6a04bdfeb4ce2d610e07a20c7bfde520d4cca86e
Garrett Grimm <ggrimm@prudentpublishing.com>
2016-03-10 22:59:34 -0800

    match notes to given frequencies

--- main.js
+++ main.js
@@ -1,89 +1,114 @@
-//https://github.com/fritzvd/signaltohertz
-function calculateHertz (frequencies, options) {
-  var rate = 22050 / 1024; // defaults in audioContext.
+var audioMidiJS = {
+	audioCtx : new (window.AudioContext || window.webkitAudioContext)(),
+	notes : ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
+	init : function () {
+		this.canvasCtx = document.querySelector('canvas').getContext("2d");
 
-	if (options) {
-		if (options.rate) {
-			rate = options.rate;
+		this.analyser = this.audioCtx.createAnalyser();
+		source = this.audioCtx.createMediaElementSource(document.querySelector('audio'));
+		source.connect(this.analyser);
+		this.analyser.connect(this.audioCtx.destination);
+		this.analyser.fftSize = 2048;
+		this.bufferLength = this.analyser.frequencyBinCount;
+
+		this.dataArray = new Uint8Array(this.bufferLength);
+		this.frequencies = new Float32Array(this.bufferLength);
+		this.noteArray = this.buildNoteArray(5);
+		this.run();
+
+		return this;
+	},
+	//https://github.com/fritzvd/signaltohertz
+	calculateHertz : function (frequencies, options) {
+	  var rate = 22050 / 1024; // defaults in audioContext.
+
+		if (options) {
+			if (options.rate) {
+				rate = options.rate;
+			}
 		}
+
+		var maxI, max = frequencies[0];
+	  
+		for (var i=0; frequencies.length > i; i++) {
+			var oldmax = parseFloat(max);
+			var newmax = Math.max(max, frequencies[i]);
+			if (oldmax != newmax) {
+				max = newmax;
+				maxI = i;
+			} 
+		}
+		return maxI * rate;
+	},
+	// http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html
+	getFrequencyByNote : function (note, octave) {
+		// note is only naturals and sharps for now
+		// Use A4 440hz as reference note
+		// Figure out how many half steps away from A4 this note is
+
+		var octaveDiff = octave - 4;
+		var distance = this.notes.indexOf(note) - this.notes.indexOf('A') + octaveDiff * 12;
+		var hertz = 440 * Math.pow(Math.pow(2, 1/12), distance);
+		return hertz;
+	},
+	// Build object of notes and frequencies [{note: 'A', octave: 1, frequency : 440}]
+	buildNoteArray : function (octaves) {
+		var noteObject = [];
+
+		for (var i = 1; i <= octaves; i++) {
+			for (var j in this.notes) {
+				noteObject.push({
+									note: this.notes[j],
+									octave: i,
+									frequency: this.getFrequencyByNote(this.notes[j], i)
+								});
+			}
+		}
+
+		return noteObject;
+	},
+	// Match frequency to closest note
+	matchNoteToFrequency : function (frequency) {
+		var closestMatch = 0; // index of NoteArray
+
+		for (var i in this.noteArray) {
+			if (Math.abs(this.noteArray[i].frequency - frequency) < Math.abs(this.noteArray[closestMatch].frequency - frequency)) {
+				closestMatch = i;
+			}
+		}
+
+		return this.noteArray[closestMatch];
+	},
+	run : function () {
+		drawVisual = requestAnimationFrame(this.run.bind(this));
+
+		this.analyser.getFloatFrequencyData(this.frequencies);
+		this.analyser.getByteTimeDomainData(this.dataArray);
+		frequency = this.calculateHertz(this.frequencies);
+
+		this.canvasCtx.fillStyle = 'rgb(0, 400, 0)';
+		this.canvasCtx.fillRect(0, 0, 500, 500);
+
+		var barWidth = (100 / this.bufferLength) * 2.5;
+		var barHeight;
+		var x = 0;
+
+		for(var i = 0; i < this.bufferLength; i++) {
+			barHeight = this.dataArray[i];
+
+			this.canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',60,60)';
+			this.canvasCtx.fillRect(x,150-barHeight/2,barWidth,barHeight/2);
+
+			x += barWidth + 1;
+		}
+
+		var note = this.matchNoteToFrequency(frequency);
+
+		document.getElementById('frequency').innerHTML = JSON.stringify(this.matchNoteToFrequency(frequency)) + ' diff: ' + (note.frequency - frequency);	
 	}
 
-	var maxI, max = frequencies[0];
-  
-	for (var i=0; frequencies.length > i; i++) {
-		var oldmax = parseFloat(max);
-		var newmax = Math.max(max, frequencies[i]);
-		if (oldmax != newmax) {
-			max = newmax;
-			maxI = i;
-		} 
-	}
-	return maxI * rate;
-}
+};
 
-var notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+audioMidiJS.init();
 
-// http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html
-// note is only naturals and sharps for now
-function getFrequencyByNote(note, octave) {
-	// Use A4 440hz as reference note
-	// Figure out how many half steps away from A4 this note is
 
-	var octaveDiff = octave - 4;
-	var distance = notes.indexOf(note) - notes.indexOf('A') + octaveDiff * 12;
-	var hertz = 440 * Math.pow(Math.pow(2, 1/12), distance);
-	return hertz;
-}
-
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var canvasCtx = document.querySelector('canvas').getContext("2d");;
-var analyser = audioCtx.createAnalyser();
-
-var myAudio = document.querySelector('audio');
-var source = audioCtx.createMediaElementSource(myAudio);
-
-source.connect(analyser);
-analyser.connect(audioCtx.destination)
-
-analyser.fftSize = 2048;
-var bufferLength = analyser.frequencyBinCount;
-var dataArray = new Uint8Array(bufferLength);
-var frequencies = new Float32Array(bufferLength);
-
-analyser.getByteTimeDomainData(dataArray);
-
-var highest = 0;
-//console.log(dataArray);
-function run() {
-  drawVisual = requestAnimationFrame(run);
-
-  analyser.getFloatFrequencyData(frequencies);
-  analyser.getByteTimeDomainData(dataArray);
-  frequency = calculateHertz(frequencies);
-
-  canvasCtx.fillStyle = 'rgb(0, 400, 0)';
-  canvasCtx.fillRect(0, 0, 500, 500);
-
-  var barWidth = (100 / bufferLength) * 2.5;
-  var barHeight;
-  var x = 0;
-
-  for(var i = 0; i < bufferLength; i++) {
-    barHeight = dataArray[i];
-
-    canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',60,60)';
-    canvasCtx.fillRect(x,150-barHeight/2,barWidth,barHeight/2);
-
-    x += barWidth + 1;
-  }
-
-  if (dataArray[0] > highest) {
-  	highest = dataArray[0];
-  }
-
-  document.getElementById('frequency').innerHTML = frequency;
-  //console.log(frequency);
-	
-}
-
-run();
